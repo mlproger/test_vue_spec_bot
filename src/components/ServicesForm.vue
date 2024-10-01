0bd316cf2d9db3738538a4d5bf6af5e92f17490a
Maxim <78304281+mlproger@users.noreply.github.com>
2024-10-01 23:46:55 +0300

    Update ServicesForm.vue

--- src/components/ServicesForm.vue
+++ src/components/ServicesForm.vue
@@ -210,10 +210,11 @@ export default {
         });
     },
     formatTime(isoString) {
-      const date = new Date(isoString);
-      const hours = date.getHours().toString().padStart(2, '0');
-      const minutes = date.getMinutes().toString().padStart(2, '0');
-      return `${hours}:${minutes}`;
+        if (!isoString) return ''; // Возврат пустой строки, если нет значения
+        const date = new Date(isoString);
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        return `${hours}:${minutes}`;
     },
     toggleEditMode() {
       this.editMode = !this.editMode;
