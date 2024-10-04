6c10755632574d4deda70b08182d86a760994ff6
Maxim <78304281+mlproger@users.noreply.github.com>
2024-10-04 10:47:29 +0300

    Update ServicesForm.vue

--- src/components/ServicesForm.vue
+++ src/components/ServicesForm.vue
@@ -266,6 +266,7 @@ export default {
         time_start: this.startTime,
         time_end: this.endTime,
         work_days: this.workingDays,
+        user_id: this.userId.toString(),
       };
       alert(dataToSend);
       try {
@@ -275,10 +276,9 @@ export default {
         this.toggleEditMode(); 
       } catch (error) {
         await axios.post(`${this.base_url}/api/v1/orders/`, dataToSend);
+        alert('Информация успешно сохранена');
         this.editMode = 0;
-        console.error('Информация успешно сохранена', error);
       }
-      alert("End");
     },
   },
 };
